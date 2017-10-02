40b810147b835b65f7978816fc6eee324b8e83cc
Tobias Derksen <t.derksen@gmx.de>
2017-10-02 16:39:48 +0200

    Changed dump script to use new filename

--- dumpdb.sh
+++ dumpdb.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [ -f scripts/latest.sql ]; then
+if [ -f scripts/10-latest.sql ]; then
   if [ -f history/$(date +%Y%m%d.sql) ]; then
     rm -f history/$(date +%Y%m%d.sql)
   fi
 
-  mv scripts/latest.sql history/$(date -r scripts/latest.sql +%Y%m%d.sql)
+  mv scripts/10-latest.sql history/$(date -r scripts/10-latest.sql +%Y%m%d.sql)
 fi
 
-pg_dump -U fmms -d fmms -s > scripts/latest.sql
+pg_dump -U fmms -d fmms -s > scripts/10-latest.sql
 
 # Delete changing of plpgsql because this will fail
-sed -i "s/COMMENT ON EXTENSION plpgsql */-- /g" scripts/latest.sql
+sed -i "s/COMMENT ON EXTENSION plpgsql */-- /g" scripts/10-latest.sql
