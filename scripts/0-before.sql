4e30dd604c304f4f09c996d56173eeb5ecf7fa06
Tobias Derksen <tobias.derksen@student.fontys.nl>
2018-01-11 18:18:28 +0100

    Fixed bug in before script

--- scripts/0-before.sql
+++ scripts/0-before.sql
@@ -8,4 +8,4 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 ALTER DATABASE modulemanagement OWNER TO module;
-DROP SCHEMA IF EXISTS users CASCADE;
+DROP SCHEMA IF EXISTS study CASCADE;
